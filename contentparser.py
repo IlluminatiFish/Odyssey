c9c94843333b6214799b88f13850ce416c1f7e2e
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2020-12-25 18:38:11 +0000

    Update contentparser.py
    Updated docustring formatting & variable conventions
    Added some javascript analysis code to execute javascript redirects

--- contentparser.py
+++ contentparser.py
@@ -1,16 +1,17 @@
+import execjs
 from bs4 import BeautifulSoup
-from utils import findURLs, getValue
+
+from utils import get_value, find_urls
 
 
-def processContent(raw_content, url):
+def process_content(raw_content, url):
     '''
-        @:purpose: Processes the raw content found in the doGET(url) function,
-                   and find HTML & Header based redirects
+        Processes the raw content found in the do_get(url) function, and find HTML & Header based redirects
 
-        @:param raw_content: The raw content taken from the doGET(url) function.
-        @:param url: The URL where the raw content is from.
+        :param raw_content: The raw content taken from the do_get(url) function.
+        :param url: The URL where the raw content is from.
 
-        @:returns next_url: Returns the next url in the redirect chain.
+        :returns: The next url in the redirect chain.
     '''
 
     content = raw_content.decode()
@@ -26,23 +27,22 @@ def processContent(raw_content, url):
 
     soup = BeautifulSoup(dom_object, 'html.parser')
 
-    if getValue('Location', http_headers):
-        print('[!] Found Location: ', getValue('Location', http_headers))
-        return getValue('Location', http_headers)
-
-
     meta_tags = soup.findAll('meta')
+    script_lines = soup.findAll('script')
+
+    if get_value('Location', http_headers):
+        return get_value('Location', http_headers)
 
     if len(meta_tags) > 0:
-
         urls = []
         for tag in soup.findAll('meta'):
-            if tag.get('content') and tag.get('http-equiv') and len(findURLs(tag.get('content'))) > 0:
+            if tag.get('content') and tag.get('http-equiv') and len(find_urls(tag.get('content'))) > 0:
                 url = tag.get('content').split('URL=')[1].replace("'", "")
                 urls.append(url)
-
         if len(urls) > 0:
-            unique_urls = set(urls) # Get rid of duplicates
+            print('[!] Found URLs in meta: ', urls)
+
+            unique_urls = set(urls)  # Get rid of duplicates
             iterator = iter(unique_urls)
             while True:
                 try:
@@ -51,3 +51,28 @@ def processContent(raw_content, url):
                     break
                 else:
                     return url
+
+    if len(script_lines) > 0:
+        for script_line in script_lines:
+            line_count = len(str(script_line).splitlines())
+            if line_count > 1:
+
+                clean_script = str(script_line).splitlines()[1: line_count - 1]
+                execution_script = ""
+
+                for js in clean_script:
+
+                    if 'document.location' in str(js) and str(js).startswith('var'):
+                        js = js.replace('document.location', '"' + url + '"')
+
+                    if 'window.location.href =' in str(js) and str(js).startswith('window.location.href ='):
+                        js = js.replace('window.location.href =', 'return')
+
+                    execution_script += js + '\n'
+
+                compiler = execjs.compile(execution_script)
+
+                try:
+                    return compiler.eval('')
+                except Exception as ex:
+                    print('An unknown JavaScript compiler error ocurred, {}'.format(ex))
