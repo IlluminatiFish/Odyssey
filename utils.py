fd818ca253fc1616da80c3a2bcfe4eed89b287d7
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2020-12-25 18:36:41 +0000

    Update utils.py
    Updated docustring formatting & variable conventions

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
-import requests
+import re, requests
 
 
-def getData(ip, type):
+def get_data(ip, type):
     '''
-        @:purpose: Get relevant data/information about a specific IP address.
+        Get relevant data/information about a specific IP address.
 
-        @:param ip: The IP address you want to get data on.
-        @:param type: The type of data you want about the IP.
+        :param ip: The IP address you want to get data on.
+        :param type: The type of data you want about the IP.
+
             Possibilities:
                 - country
                 - countryCode
@@ -21,7 +22,7 @@ def getData(ip, type):
                 - isp
                 - org
 
-        @:returns request.json[type]: The type of data about the IP you requested.
+        :returns: The type of data about the IP you requested.
     '''
 
     request = requests.get('http://ip-api.com/json/{}'.format(ip))
@@ -32,35 +33,46 @@ def getData(ip, type):
 
 def dictionarize(request_data):
     '''
-        @:purpose: Convert the HTTP response of a web server to a dictionary
-                   for ease of use.
+        Convert the HTTP response of a web server to a dictionary for ease of use.
 
-        @:param request_data: The response data from the web server.
+        :param request_data: The response data from the web server.
 
-        @:returns request_headers: The request headers as a dictionary
+        :returns: The request headers as a dictionary
     '''
 
     request_headers = {}
     for splitter in request_data:
         # Quick patch, that checks if the header entry exists
         if splitter:
-            # Quick patch, with title() to capitalize all header entries
+            # Quick patch with title() to capitalize all header entries
             request_headers[splitter.split(': ')[0].title()] = splitter.split(': ')[1]
 
     return request_headers
 
 
-def getValue(key, request_data):
+def get_value(key, request_data):
     '''
-        @:purpose: Get a header key from the dictionarized request response list.
+        Get a header key from the dictionarized request response list.
 
-        @:param key: The key you want to get the data from in the headers.
-        @:param request_data: The response data from the web server.
+        :param key: The key you want to get the data from in the headers.
+        :param request_data: The response data from the web server.
 
-        @:returns dictionarize(request_data)[key]: The value in the header @:param key
+        :returns: The value in the header :param key:
     '''
 
     try:
         return dictionarize(request_data)[key]
     except KeyError:
         return
+
+def find_urls(data):
+    '''
+        Gets all the strings that match the regex pattern for URLs.
+
+        :param data: The string/data you want to check to see if URLs exist in them.
+
+        :returns: The list of strings that match the regex pattern in :param data:
+    '''
+
+    regex = re.compile('http[s]?://(?:[\w]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.UNICODE)
+    return regex.findall(data)
