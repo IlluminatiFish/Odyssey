cc5ad5fe130c46717797fdb78dc6811928926f63
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-01-26 18:43:23 +0000

    Update contentparser.py
    - Fixed a case where the next URL was seen to be a local ip / resolved to a local ip in the location header of the previous URL

--- contentparser.py
+++ contentparser.py
@@ -1,4 +1,6 @@
-import re, execjs
+import re, execjs, socket
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 
 from utils import get_value, find_urls
@@ -30,11 +32,29 @@ def process_content(raw_content, url):
 
     # Changed from findAll() to find_all() to iterate over the rest of the tags
     meta_tags = soup.find_all('meta')
-    script_lines = soup.find_all('script')  
-
+    script_lines = soup.find_all('script')
 
     if get_value('Location', http_headers):
-        return get_value('Location', http_headers)
+        object = urlparse(get_value('Location', http_headers)).netloc
+
+        ip = None
+        domain = None
+
+        object_match = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', object)
+
+        if object_match != None:
+            ip = object
+        else:
+            domain = object
+            domain_ip = None
+            try:
+                domain_ip = str(socket.gethostbyname(domain))
+            except Exception as exc:
+                print('[-] An error ocurred in the content parser:\n\n', exc)
+
+        # Fixes a case where the next url was seen to be a local ip / resolved to a local ip in the location header of the previous url
+        if ip != '127.0.0.1' and domain_ip != '127.0.0.1':
+            return get_value('Location', http_headers)
 
     if len(meta_tags) > 0:
         urls = []
@@ -55,9 +75,12 @@ def process_content(raw_content, url):
                     return url
 
     if len(script_lines) > 0:
+
         for script_line in script_lines:
 
+
             line_count = len(str(script_line).splitlines())
+
 
             if line_count > 1:
 
@@ -66,10 +89,15 @@ def process_content(raw_content, url):
 
                 execution_script = ""
 
+
                 for js in clean_script:
+                    #print('js:', js)
 
                     if 'document.location' in str(js) and str(js).startswith('var'):
                         js = js.replace('document.location', '"' + url + '"')
+
+                    if 'window.location=' in str(js) and str(js).startswith('window.location='):
+                        js = js.replace('window.location=', 'return ')
 
                     if 'window.location.href =' in str(js) and str(js).startswith('window.location.href ='):
                         js = js.replace('window.location.href =', 'return')
@@ -83,11 +111,17 @@ def process_content(raw_content, url):
                     if 'document.location.href.replace' in str(js):
                         js = js.replace('document.location.href', '"' + url + '"')
 
+                    if 'window.location.replace' in str(js) and str(js).startswith('window.location.replace'):
+                        js = js.replace('window.location.replace', 'return ')
+
                     execution_script += js + '\n'
+
+                #print(execution_script)
 
                 compiler = execjs.compile(execution_script)
 
                 try:
+                    print(compiler.eval(''))
                     return compiler.eval('')
                 except Exception as ex:
                     continue # Added a 'continue' statement to iterate over the rest of the scripts found marked by <script> tags
@@ -95,6 +129,7 @@ def process_content(raw_content, url):
 
             # Regex pattern to get rid of any HTML tags, better than the old method used
             clean_script = re.sub('<.*?>', '', str(script_line))
+
 
             if len(clean_script) > 0:
 
@@ -105,6 +140,13 @@ def process_content(raw_content, url):
 
                 if 'window.location = ' in str(clean_script) and str(clean_script).startswith('window.location = '):
                     script += clean_script.replace('window.location = ', 'return ')
+
+                if 'window.location.replace' in str(clean_script) and str(clean_script).startswith('window.location.replace'):
+                    script += clean_script.replace('window.location.replace', 'return ')
+
+                if 'top.location.href=' in str(clean_script) and str(clean_script).startswith('top.location.href='):
+                    script += clean_script.replace('top.location.href=', 'return ')
+
 
                 script += ';'
 
