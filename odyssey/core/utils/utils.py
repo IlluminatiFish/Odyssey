301fd56c6135c20d890adee77cd61bbcc9f3b8af
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-10-09 15:48:00 +0100

    Fixed v1.2 updated release
    Fixed url finding regex

--- odyssey/core/utils/utils.py
+++ odyssey/core/utils/utils.py
@@ -35,7 +35,7 @@ class HeaderUtils:
 
 
 def find_urls(data):
-    pattern = re.compile('^(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:/?|[/?]\S+)$')
+    pattern = re.compile('(https?:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(:?\d*)\/?([a-z_\/0-9\-#.]*)\??([a-z_\/0-9\-#=&?%.]*)')
     return pattern.findall(data)
 
 
